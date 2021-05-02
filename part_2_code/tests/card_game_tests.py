1d8f3f6c57d25c9fd62b9ff7fd23285e5a09b046
Spencer <Spencer.Taber@yahoo.com>
2021-05-02 13:39:11 +0100

    test highest card error 1

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -5,12 +5,23 @@ from src.card_game import CardGame
 class TestCardGame(unittest.TestCase):
 
     def setUp(self):
-        self.ace = Card(spades, 1)
-        self.club = Card(clubs, 5)
+        self.ace = Card("spades", 1)
+        self.club = Card("clubs", 5)
     
 
     def test_check_for_ace(self):
         self.assertEqual(True, self.ace.value)
+    
+    # def test_check_for_ace_false(self):
+    #     self.assertEqual(False, self.club.value)
+
+    def test_highest_card1(self):
+        card1 = self.club.value
+        card2 = self.ace.value
+        self.assertEqual(card1, def_highest_card(card1,card2) )
+
+
+
     
 
     
